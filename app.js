f07f5ad743b19c567fbc68e7a15f303630df968a
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-31 12:00:59 -0300

    Criar rota home-hello

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
 // https://www.youtube.com/watch?v=qwfE7fSVaZM
 // 9'21''
 
-console.log('Task Manager App')
+const express = require('express');
+
+
+
+const app = express();
+
+
+
+// ROUTES
+// app.get('/api/v1/tasks')
+// app.post('/api/v1/tasks')
+// app.get('/api/v1/tasks/:id')
+// app.patch('/api/v1/task/:id')
+// app.delete('/api/v1/task/:id')
+app.get('/hello', (req, res) => {
+	res.send('Task manager App');
+});
+
+
+
+
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
