2dece4b0e9c6ee7c609a0446c2f81e3fa29979be
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-01 20:27:10 -0300

    Refatorando as rotas GET tasks, GET task-id e DELETE task-id inserindo DB e tratamento de erros.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,8 +3,17 @@ const Task = require('../models/task');
 
 
 
-const getAllTasks = (req, res) => {
-	res.json('Get all tasks');
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find( {} );
+		
+		res.status(200).json({ tasks });
+	} catch (error) {
+		console.log(error);
+		
+		res.status(500).json({ msg: error });
+	}
+	
 };
 
 const createTask = async(req, res) => {
@@ -12,7 +21,7 @@ const createTask = async(req, res) => {
 		const task = await Task.create(req.body);
 		
 		// 201 - Successful POST
-		res.status(201).json( task );
+	res.status(201).json({ task });
 	} catch (error) {
 		console.log(error);
 		
@@ -20,16 +29,44 @@ const createTask = async(req, res) => {
 	}
 };
 
-const getTask = (req, res) => {
-	res.json({ "id": req.params.id });
+const getTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOne({ _id: taskID });
+		
+		if (!task) { 
+			return res.status(404).json({ msg: `No task with id ${taskID}` }); 
+		}
+		
+		res.status(200).json( task );
+	} catch (error) {
+		console.log(error);
+		
+		res.status(500).json({ msg: error });
+	}
 };
 
 const updateTask = (req, res) => {
 	res.send('Update task');
 };
 
-const deleteTask = (req, res) => {
-	res.send('Delete task');
+const deleteTask = async (req, res) => {
+	try {
+		const {id: taskID } = req.params;
+		const task = await Task.findOneAndDelete({ _id: taskID });
+		
+		if (!task) {
+			res.status(404).json({ msg: `No task with id ${taskID}` });
+		}
+		
+		//res.status(200).json({ task });
+		//res.status(200).json({ task: null, status: 'success' });
+		res.status(200).send();
+	} catch (error) {
+		console.log(error);
+		
+		res.status(500).json({ msg: error });
+	}
 };
 
 
